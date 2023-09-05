43373e1820a3fa79c7f2529a9a352e4f30d23017
Juan Fran <juanframaro@gmail.com>
2023-09-05 11:28:08 +0100

    fix: changing the order of the urls

--- main_windguru.py
+++ main_windguru.py
@@ -2,10 +2,10 @@ from front import plot_forecast, plot_tides
 
 if __name__ == "__main__":
     urls = [
+        "https://www.windguru.cz/586104",  # costa teguise
         "https://www.windguru.cz/49326",  # Jameos del agua
         "https://www.windguru.cz/49325",  # La garita
         "https://www.windguru.cz/49328",  # famara
-        "https://www.windguru.cz/586104",  # costa teguise
     ]
 
     plot_forecast(urls)
