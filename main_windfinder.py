8bf4a0e865e050a74485d4589e440bb8b57c1bfa
Juan Fran Martín <jfmartinr@tirant.com>
2023-09-13 20:50:47 +0100

    fix: windfinder remove duplicate urls

--- main_windfinder.py
+++ main_windfinder.py
@@ -2,17 +2,11 @@ from front import plot_forecast, plot_tides
 
 if __name__ == "__main__":
     urls = [
-        "https://es.windfinder.com/forecast/la_palma1",
         "https://es.windfinder.com/forecast/marina_rubicon_canary_islands_spain",
         "https://es.windfinder.com/forecast/playa_blanca_canary_islands_spain_marina",
         "https://es.windfinder.com/forecast/punta-pechiguera-playa-blanca",
-        "https://es.windfinder.com/forecast/lanzarote_playa_de_las_cucharas_costa_te",
         "https://es.windfinder.com/forecast/lanzarote_puerto_del_carmen_costa_teguis",
-        "https://es.windfinder.com/forecast/costa_teguise_las_cucharas",
         "https://es.windfinder.com/forecast/lanzarote_playa_honda_costa_teguise",
-        "https://es.windfinder.com/forecast/lanzarote",
-        "https://es.windfinder.com/forecast/arrecife_playa_honda",
-        "https://es.windfinder.com/forecast/arrecife_puerto",
     ]
 
     plot_forecast(urls)
