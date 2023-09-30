c748e1cb15d6e833d3d0e2dc3379e1ac233d3006
Juan Fran Martín <jfmartinr@tirant.com>
2023-09-30 01:57:28 +0100

    fix: remove arrieta from sforecast

--- urls/sforecast.py
+++ urls/sforecast.py
@@ -1,7 +1,6 @@
 SFORECAST_URLS = [
     "https://www.surf-forecast.com/breaks/Arrieta/forecasts/latest/six_day",
     "https://www.surf-forecast.com/breaks/Caletade-Cabello/forecasts/latest/six_day",
-    "https://www.surf-forecast.com/breaks/Jameosdel-Agua/forecasts/latest/six_day",
     "https://www.surf-forecast.com/breaks/La-Santa-The-Slab/forecasts/latest/six_day",
     "https://www.surf-forecast.com/breaks/Playade-Famara_1/forecasts/latest/six_day",
     "https://www.surf-forecast.com/breaks/San-Juan/forecasts/latest/six_day",
