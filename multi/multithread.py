188542592afb39e2258e3ea0ed76749134fef571
Juan Fran Martín <jfmartinr@tirant.com>
2023-10-02 18:41:23 +0100

    fix: only a browser

--- multi/multithread.py
+++ multi/multithread.py
@@ -9,10 +9,14 @@ from time import sleep
 def scrape_multiple_browser(urls, _object):
     forecast = pd.DataFrame()
     results = []
-    with utils.open_browser() as browser:
+    try:
+        browser = utils.open_browser()
         for index, url in enumerate(urls):
             result = _object.scrape(browser, url, index)
             results.append(result)
+    finally:
+        browser.close()
+        browser.quit()
 
     for url, content in zip(urls, results):
         df = utils.handle_wind(content)
