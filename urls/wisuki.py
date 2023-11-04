89a4da838e9b3c0c0b86f52f016f5c76eb025f8e
Juan Fran Martín <jfmartinr@tirant.com>
2023-11-04 19:22:43 +0000

    feat: add la garita

--- urls/wisuki.py
+++ urls/wisuki.py
@@ -6,7 +6,7 @@ WISUKI_URLS = [
         #"https://es.wisuki.com/forecast/2842/playa-del-risco",
         #"https://es.wisuki.com/forecast/5987/orzola",
         #"https://es.wisuki.com/forecast/2834/jameos-del-agua",
-        #"https://es.wisuki.com/forecast/2841/playa-de-la-garita",
+        "https://es.wisuki.com/forecast/2841/playa-de-la-garita",
         "https://es.wisuki.com/forecast/7183/playa-jablillo",
         "https://es.wisuki.com/forecast/2844/playa-honda",
         "https://es.wisuki.com/forecast/2839/matagorda",
