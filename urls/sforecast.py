d3be115f055059f6ab7c58e78ec44979a7d2b8e2
Juan Fran Martín <jfmartinr@tirant.com>
2023-11-21 21:38:31 +0000

    feat: adding more urls to sforecast

--- urls/sforecast.py
+++ urls/sforecast.py
@@ -5,4 +5,8 @@ SFORECAST_URLS = [
     "https://www.surf-forecast.com/breaks/Playade-Famara_1/forecasts/latest/six_day",
     "https://www.surf-forecast.com/breaks/San-Juan/forecasts/latest/six_day",
     "https://www.surf-forecast.com/breaks/Puntade-Mujeres/forecasts/latest/six_day"
+    "https://www.surf-forecast.com/breaks/Playa-de-la-Cera-Playa-Blanca/forecasts/latest/six_day",
+    "https://www.surf-forecast.com/breaks/El-Tiburon-Playa-Honda/forecasts/latest/six_day",
+    "https://surf-forecast.com/breaks/Playa-Bastian-Costa-Teguise/forecasts/latest/six_day",
+    "https://www.surf-forecast.com/breaks/Castillo-del-Rubicon-Playa-Blanca/forecasts/latest/six_day",
 ]
