0e9a10f57860ed8d79382851ed31648bc11d4e6b
Juan Fran Martín <jfmartinr@tirant.com>
2023-12-22 20:37:57 +0000

    update: comment lasanta url in windguru

--- urls/windguru.py
+++ urls/windguru.py
@@ -1,17 +1,17 @@
 WINDGURU_URLS = [
-    #"https://www.windguru.cz/586104",  # costa teguise
-    "https://www.windguru.cz/49323", # las cucharas
-    #"https://www.windguru.cz/49326",  # Jameos del agua
-    #"https://www.windguru.cz/49325",  # La garita
-    "https://www.windguru.cz/207014", #la garita
+    # "https://www.windguru.cz/586104",  # costa teguise
+    "https://www.windguru.cz/49323",  # las cucharas
+    # "https://www.windguru.cz/49326",  # Jameos del agua
+    # "https://www.windguru.cz/49325",  # La garita
+    "https://www.windguru.cz/207014",  # la garita
     "https://www.windguru.cz/49328",  # famara
-    #"https://www.windguru.cz/501282", # janubio
-    "https://www.windguru.cz/49319", # playa dorada
-    #"https://www.windguru.cz/430882", # puerto calero
-    "https://www.windguru.cz/49320", # los pocillos
-    "https://www.windguru.cz/49321", # matagorda
-    "https://www.windguru.cz/49322", # playa honda
-    #"https://www.windguru.cz/49324", # los charcos
-    "https://www.windguru.cz/501281", #la santa
-    "https://www.windguru.cz/830259", #arrecife
+    # "https://www.windguru.cz/501282", # janubio
+    "https://www.windguru.cz/49319",  # playa dorada
+    # "https://www.windguru.cz/430882", # puerto calero
+    "https://www.windguru.cz/49320",  # los pocillos
+    "https://www.windguru.cz/49321",  # matagorda
+    "https://www.windguru.cz/49322",  # playa honda
+    # "https://www.windguru.cz/49324", # los charcos
+    # "https://www.windguru.cz/501281", #la santa
+    "https://www.windguru.cz/830259",  # arrecife
 ]
