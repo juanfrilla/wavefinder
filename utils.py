6358b78c71f3a39ab904be2835defd05742b7698
Juan Fran Martín <jfmartinr@tirant.com>
2024-04-05 19:40:20 +0100

    fix: conditions

--- utils.py
+++ utils.py
@@ -51,8 +51,8 @@ def separate_spots(df: pl.DataFrame):
     df = df.with_columns(
         pl.when(
             (
-                (pl.col("wind_direction_predominant") == "NE")
-                | (pl.col("wind_direction").str.contains("NE"))
+                (pl.col("wind_direction_predominant").str.contains("N"))
+                | (pl.col("wind_direction").str.contains("N"))
             )
             & (pl.col("wave_direction").str.contains("N"))
             & (pl.col("wind_speed") >= 19.0)
